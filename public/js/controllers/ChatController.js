72ecc7847b691164e91cd51704a73dfe3ba1ad02
Chubasik <tsygankov.maxim@gmail.com>
2018-11-25 14:57:54 +0300

    <hackaton-chubasik> Working chat model

--- public/js/controllers/ChatController.js
+++ public/js/controllers/ChatController.js
@@ -1,9 +1,16 @@
 import EventBus from '../lib/eventbus/eventbus';
 import ChatView from '../views/chat/ChatView';
+import ChatModel from '../models/ChatModel';
+
+const eventList = [
+    'sendMessage',
+    'messageReceived'
+];
 
 export default class ChatController {
     constructor () {
-        const eventBus = new EventBus([]);
+        const eventBus = new EventBus(eventList);
         this.chatView = new ChatView({ eventBus });
+        this.chatModel = new ChatModel({ eventBus });
     }
 }
--- public/js/views/chat/ChatView.js
+++ public/js/views/chat/ChatView.js
@@ -5,23 +5,36 @@ import ChatMessage from '../../components/chatMessage/chatMessage';
 export default class ChatView extends View {
     constructor ({ eventBus = {} } = {}) {
         super(template, eventBus);
+        this._eventBus.subscribeToEvent('messageReceived', this._messageReceived.bind(this));
+
         this._messages = [];
     }
 
     render (root) {
         super.render(root);
         const chat = document.querySelector('.chat');
-
         const testMessage0 = new ChatMessage({
             message: 'hi'
         });
-
         testMessage0.appendToChat(chat);
-
         const testMessage1 = new ChatMessage({
             message: 'wasup'
         });
-
         testMessage1.appendToChat(chat);
+
+        const sendButton = document.querySelector('.chat__send-button');
+        sendButton.addEventListener('click', this._onSendClick.bind(this));
+    }
+
+    _messageReceived(message) {
+        const chat = document.querySelector('.chat');
+        const newMessage = new ChatMessage({ message });
+        newMessage.appendToChat(chat);
+    }
+
+    _onSendClick () {
+        const textField = document.querySelector('.chat__text-field');
+        this._eventBus.triggerEvent('sendMessage', textField.value);
+        textField.value = '';
     }
 }
