3702db5888bc727172acb618f94d32dafff04fee
Sinimawath <sinimawath@gmail.com>
2018-11-30 01:07:10 +0300

    <refactoring> fix timer -

--- public/js/components/timer/timer.js
+++ public/js/components/timer/timer.js
@@ -2,52 +2,52 @@ import './timer.less';
 
 const noop = () => null;
 const SECOND = 1000;
-const TIMER_ACTIVE_CLASS = "timer_active";
+const TIMER_ACTIVE_CLASS = 'timer_active';
 
 export default class Timer {
-    constructor({duration = 600, root = null, timerExpireCallback = noop} = {}) {
+    constructor ({ duration = 600, root = null, timerExpireCallback = noop } = {}) {
         this._root = root;
         this._duration = +duration;
         this._current = +duration;
         this._timerExpireCallback = timerExpireCallback;
     }
 
-    stop() {
+    stop () {
         clearInterval(this._timerID);
         this._output.classList.remove(TIMER_ACTIVE_CLASS);
     }
 
-    start() {
+    start () {
         this._output.classList.add(TIMER_ACTIVE_CLASS);
-        this._update({current: this._formatSecondToTime({seconds: this._current--})});
-        this._timerID = setInterval(()=> {
-            this._update({current: this._formatSecondToTime({seconds: this._current--})});
+        this._update({ current: this._formatSecondToTime({ seconds: this._current-- }) });
+        this._timerID = setInterval(() => {
             if (this._current < 0) {
                 this._timerExpireCallback();
                 this.stop();
+                return;
             }
+            this._update({ current: this._formatSecondToTime({ seconds: this._current-- }) });
         }, SECOND);
     }
 
-    render() {
+    render () {
         this._root.innerHTML = `<div class="timer"></div>`;
-        this._output = this._root.querySelector(".timer");
-        this._update({current: this._formatSecondToTime({seconds: this._duration})});
+        this._output = this._root.querySelector('.timer');
+        this._update({ current: this._formatSecondToTime({ seconds: this._duration }) });
     }
 
-    _update({current}) {
+    _update ({ current }) {
         this._output.innerHTML = current;
     }
 
-    _formatSecondToTime({seconds}) {
-        if (typeof seconds !== "number") {
-            return "";
+    _formatSecondToTime ({ seconds }) {
+        if (typeof seconds !== 'number') {
+            return '';
         }
 
-        let minute = ~~(seconds/60);
-        let second = seconds%60;
+        let minute = ~~(seconds / 60);
+        let second = seconds % 60;
 
-        return ("0" + minute).slice(-2) + ":" + ("0" + second).slice(-2);
+        return ('0' + minute).slice(-2) + ':' + ('0' + second).slice(-2);
     }
-
 };
