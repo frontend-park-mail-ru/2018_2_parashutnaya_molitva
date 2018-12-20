ad6f07acef0415eaf4eee593e95d504ea7ae1655
Sinimawath <sinimawath@gmail.com>
2018-12-20 20:20:51 +0300

    <profile-fix> profile landing

--- public/js/views/profile/ProfileView.js
+++ public/js/views/profile/ProfileView.js
@@ -9,11 +9,9 @@ export default class ProfileView extends View {
         this._eventBus.subscribeToEvent('checkAuthResponse', this._onCheckAuthResponse.bind(this));
         this._eventBus.subscribeToEvent('loadUserResponse', this._onLoadUserResponse.bind(this));
 
-        this._eventBus.subscribeToEvent('changeEmailResponse', this._onChangeEmailResponse.bind(this));
         this._eventBus.subscribeToEvent('changePasswordResponse', this._onChangePassResponse.bind(this));
         this._eventBus.subscribeToEvent('changeAvatarResponse', this._onChangeAvatarResponse.bind(this));
         this._eventBus.subscribeToEvent('changeAvatarSuccess', this._onChangeAvatarSuccess.bind(this));
-        this._eventBus.subscribeToEvent('submitEmailSuccess', this._onSubmitEmailSucces.bind(this));
         this._eventBus.subscribeToEvent('submitPasswordSuccess', this._onSubmitPasswordSucces.bind(this));
     }
 
@@ -69,20 +67,8 @@ export default class ProfileView extends View {
         this._avatarUploader = this.el.querySelector('.js-upload-avatar');
         this._avatarUploaderWarning = this.el.querySelector('.js-warning-avatar');
 
-        this._emailBlock = this.el.querySelector('.js-email-row');
-        // this._emailEditButton = this._emailBlock.querySelector('button');
-        this._emailField = this._emailBlock.querySelector('.js-email-field');
-
-        this._emailFormWrapper = this.el.querySelector('.js-email-form');
-        this._emailForm = this._emailFormWrapper.querySelector('form');
-        this._emailWarning = this._emailFormWrapper.querySelector('.js-warning-email');
-
-        this._emailSubmitButton = this._emailForm.querySelector('.js-button-submit');
-        this._emailCancelButton = this._emailForm.querySelector('.js-button-cancel');
-        this._emailInputEmailForm = this._emailForm.elements['email'];
-
         this._passwordBlock = this.el.querySelector('.js-password-row');
-        this._passwordEditButton = this._passwordBlock.querySelector('button');
+        this._passwordEditButton = this._passwordBlock;
 
         this._passwordFormWrapper = this.el.querySelector('.js-password-form');
         this._passwordForm = this._passwordFormWrapper.querySelector('form');
@@ -99,21 +85,6 @@ export default class ProfileView extends View {
     _initElementsEvents () {
         this._avatarUploader.addEventListener('change', () => {
             this._eventBus.triggerEvent('changeAvatar', { avatar: this._avatarUploader.files[0] });
-        });
-
-        // this._emailEditButton.addEventListener('click', () => {
-        //     this._showElement(this._emailFormWrapper);
-        //     this._hideElement(this._emailBlock);
-        // });
-        this._emailCancelButton.addEventListener('click', (ev) => {
-            ev.preventDefault();
-            this._hideElement(this._emailFormWrapper);
-            this._showElement(this._emailBlock);
-        });
-
-        this._emailSubmitButton.addEventListener('click', (ev) => {
-            ev.preventDefault();
-            this._eventBus.triggerEvent('submitEmail', { email: this._emailInputEmailForm.value });
         });
 
         this._passwordEditButton.addEventListener('click', () => {
@@ -133,23 +104,11 @@ export default class ProfileView extends View {
     }
 
     _initEventBusEvents () {
-        this._emailInputEmailForm.addEventListener('input',
-            this._onChangeEmail.bind(this, this._emailInputEmailForm));
 
         this._passwordInputPasswordForm.addEventListener('change',
             this._onChangePass.bind(this, this._passwordInputPasswordForm));
     }
 
-    _onSubmitEmailSucces (data) {
-        this._hideElement(this._emailFormWrapper);
-        this._showElement(this._emailBlock);
-
-        if (!data.email) {
-            return;
-        }
-
-        this._emailField.innerHTML = data.email;
-    }
 
     _onSubmitPasswordSucces (data) {
         this._hideElement(this._passwordFormWrapper);
@@ -161,18 +120,10 @@ export default class ProfileView extends View {
         this._eventBus.triggerEvent('changePassword', { pass });
     }
 
-    _onChangeEmail (emailEl) {
-        const email = emailEl.value;
-        this._eventBus.triggerEvent('changeEmail', { email });
-    }
-
     _onChangePassResponse (data) {
         this._onChangeResponseTmpl(data.error, this._passwordInputPasswordForm, this._passWarning);
     }
 
-    _onChangeEmailResponse (data) {
-        this._onChangeResponseTmpl(data.error, this._emailInputEmailForm, this._emailWarning);
-    }
 
     _onChangeResponseTmpl (error, el, warning) {
         if (error) {
