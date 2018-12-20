9eff609c8532f0ce13cb6fe074b995d7ce186b5e
Sinimawath <sinimawath@gmail.com>
2018-12-21 01:21:56 +0300

    <profile-fix> add move highlight

--- public/js/views/game/GameView.js
+++ public/js/views/game/GameView.js
@@ -1,6 +1,6 @@
 import View from '../../lib/view';
 import Board from '../../components/chess/board/board';
-import {GAME} from '../../lib/eventbus/events';
+import { GAME } from '../../lib/eventbus/events';
 
 export default class GameView extends View {
     constructor ({ eventBus = {} } = {}) {
@@ -15,21 +15,21 @@ export default class GameView extends View {
     render (root, data = {}) {
         this._root = root;
         this._board = new Board({ moveCallback: this._moveCallback.bind(this), sideOfView: data.color });
-        this._board.render(root);
+        this._board.render(root, { move: data.move });
     }
 
-    update () {
-        this._board.render(this._root);
+    update ({ move = '' } = {}) {
+        this._board.render(this._root, { move });
     }
 
     _moveCallback (move) {
-        this._eventBus.triggerEvent(GAME.MOVE, {move});
+        this._eventBus.triggerEvent(GAME.MOVE, { move });
     }
 
-    _onMoveSuccess ({state, turn} = {}) {
+    _onMoveSuccess ({ state, turn, move } = {}) {
         console.log('making a move', state);
         this._board.setState({ boardState: state, turn: turn });
-        this.update();
+        this.update({ move });
     }
 
     _onMoveFailure () {
@@ -37,6 +37,6 @@ export default class GameView extends View {
     }
 
     _onGameOver (loser) {
-        console.log(`gameover ${+loser === 1 ? 'white': 'black'} lost`);
+        console.log(`gameover ${+loser === 1 ? 'white' : 'black'} lost`);
     }
 }
