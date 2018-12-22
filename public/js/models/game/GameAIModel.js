7cb59f369d6901da1a50a4a1fc81da779e52b1d1
Chubasik <tsygankov.maxim@gmail.com>
2018-12-22 15:57:16 +0300

    <tf-worker> Add working model

--- public/js/models/game/GameAIModel.js
+++ public/js/models/game/GameAIModel.js
@@ -106,64 +106,6 @@ export default class GameAIModel {
             // }
 
             // #### Tensorflow ####
-
-            tf.loadModel('keras/model.json').then(model => {
-                const legalMoves = this._game._board.legalMoves(this._game._turn);
-                const legalMovesKeys = Object.keys(legalMoves);
-                const minimaxScores = [];
-
-                const scoreWeights = [];
-
-                // AI.shuffleMoves(legalMovesKeys);
-
-                legalMovesKeys.forEach(move => {
-                    const newScore = AI.minimaxTreeScoreModel(legalMoves[move], TREE_DEPTH - 1, !this._game.turn(), model);
-                    minimaxScores.push(newScore);
-                    const weight = AI.minimaxTreeScoreModel(legalMoves[move], 0, !this._game.turn(), model);
-                    scoreWeights.push(weight);
-                });
-
-                console.log('minimax', minimaxScores);
-                console.log('weights', scoreWeights);
-
-                const bestScore = Math.min(...minimaxScores);
-                const bestMoveIndices = [];
-                const bestMoveWeights = [];
-                for (let i = 0; i < minimaxScores.length; i++) {
-                    const score = minimaxScores[i];
-                    if (score === bestScore) {
-                        bestMoveIndices.push(i);
-                        bestMoveWeights.push(scoreWeights[i]);
-                    }
-                }
-
-                const bestWeightIndex = bestMoveWeights.indexOf(Math.min(...bestMoveWeights));
-                const bestMoveIndex = bestMoveIndices[bestWeightIndex];
-
-
-
-                // console.log('best moves', bestMoveIndices);
-                // const bestMoveIndex = bestMoveIndices[Math.floor(Math.random() * bestMoveIndices.length)];
-
-                // const bestMoveIndex = minimaxScores.indexOf(Math.min(...minimaxScores));
-                const bestMove = legalMovesKeys[bestMoveIndex];
-
-                console.log(minimaxScores);
-                console.log(legalMovesKeys);
-                console.log(bestMoveIndex);
-                console.log(bestMove);
-                return Promise.resolve(bestMove);
-            }).then(bestMove => {
-                console.log('last then', bestMove);
-                this._game.move(bestMove);
-
-                this._eventBus.triggerEvent(GAME.MOVE_SUCCESS, { state: this._game.boardString(),
-                    turn: this._game.turn(),
-                    deadPiece: this._game.findNewDeadPiece() });
-                if (this._game.isGameOver()) {
-                    this._eventBus.triggerEvent(GAME.GAMEOVER, { turn: this._game.turn() });
-                }
-            });
         } else if (this._game.isLegalPromotionMove(move)) {
             this._promotionMove = move;
             this._eventBus.triggerEvent(GAME.PROMOTION, { turn: this._game.turn() });
--- public/js/models/workers/aiWorkerModel.js
+++ public/js/models/workers/aiWorkerModel.js
@@ -2,6 +2,7 @@
 import Game from '../../lib/chess/game';
 import AI from '../../lib/chess/ai';
 import * as tf from '@tensorflow/tfjs';
+import { PIECE_COLOR } from '../../lib/chess/enums';
 
 export default class AiModel {
     constructor (mode) {
@@ -45,32 +46,83 @@ export default class AiModel {
     }
 
     moveThirdMode (move) {
-
         return tf.loadModel('keras/model.json').then(model => {
+            const TREE_DEPTH = 2;
             this._game.move(move);
             const legalMoves = this._game._board.legalMoves(this._game._turn);
             const legalMovesKeys = Object.keys(legalMoves);
-            const oneHotBoards = [];
+            const minimaxScores = [];
+
+            const scoreWeights = [];
+
+            // AI.shuffleMoves(legalMovesKeys);
+
             legalMovesKeys.forEach(move => {
-                const oneHotBoard = AI.boardToOneHot(legalMoves[move]);
-                oneHotBoards.push(oneHotBoard);
+                const newScore = AI.minimaxTreeScoreModel(legalMoves[move], TREE_DEPTH - 1, !this._game.turn());
+                minimaxScores.push(newScore);
+                const weight = AI.minimaxTreeScoreModel(legalMoves[move], 0, !this._game.turn(), model);
+                scoreWeights.push(weight);
             });
-            const boardsTensor = tf.tensor(oneHotBoards);
 
-            console.log('model loaded');
-            const predictions = model.predict(boardsTensor);
-            const predictionsFloat32Array = predictions.dataSync();
-            const predictionsArray = Array.from(predictionsFloat32Array);
+            console.log('minimax', minimaxScores);
+            console.log('weights', scoreWeights);
 
-            const bestMoveIndex = predictionsArray.indexOf(Math.min(...predictionsArray));
+            const bestScore = Math.min(...minimaxScores);
+            const bestMoveIndices = [];
+            const bestMoveWeights = [];
+            for (let i = 0; i < minimaxScores.length; i++) {
+                const score = minimaxScores[i];
+                if (score === bestScore) {
+                    bestMoveIndices.push(i);
+                    bestMoveWeights.push(scoreWeights[i]);
+                }
+            }
+
+            const bestWeightIndex = bestMoveWeights.indexOf(Math.min(...bestMoveWeights));
+            const bestMoveIndex = bestMoveIndices[bestWeightIndex];
+
+
+
+            // console.log('best moves', bestMoveIndices);
+            // const bestMoveIndex = bestMoveIndices[Math.floor(Math.random() * bestMoveIndices.length)];
+
+            // const bestMoveIndex = minimaxScores.indexOf(Math.min(...minimaxScores));
             const bestMove = legalMovesKeys[bestMoveIndex];
 
-            console.log(predictionsArray);
+            console.log(minimaxScores);
             console.log(legalMovesKeys);
             console.log(bestMoveIndex);
             console.log(bestMove);
+
             this._game.move(bestMove);
             return bestMove;
         }).catch((err) => console.log(err));
+
+        // return tf.loadModel('keras/model.json').then(model => {
+        //     this._game.move(move);
+        //     const legalMoves = this._game._board.legalMoves(this._game._turn);
+        //     const legalMovesKeys = Object.keys(legalMoves);
+        //     const oneHotBoards = [];
+        //     legalMovesKeys.forEach(move => {
+        //         const oneHotBoard = AI.boardToOneHot(legalMoves[move]);
+        //         oneHotBoards.push(oneHotBoard);
+        //     });
+        //     const boardsTensor = tf.tensor(oneHotBoards);
+        //
+        //     console.log('model loaded');
+        //     const predictions = model.predict(boardsTensor);
+        //     const predictionsFloat32Array = predictions.dataSync();
+        //     const predictionsArray = Array.from(predictionsFloat32Array);
+        //
+        //     const bestMoveIndex = predictionsArray.indexOf(Math.min(...predictionsArray));
+        //     const bestMove = legalMovesKeys[bestMoveIndex];
+        //
+        //     console.log(predictionsArray);
+        //     console.log(legalMovesKeys);
+        //     console.log(bestMoveIndex);
+        //     console.log(bestMove);
+        //     this._game.move(bestMove);
+        //     return bestMove;
+        // }).catch((err) => console.log(err));
     }
 }
